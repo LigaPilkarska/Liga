254db492b701154b7de5db17428b46746f3eb00a
Tadeusz Cekala <eltadziko@gmail.com>
2012-03-05 15:41:22 +0100

    Dodano wyswietlanie newsow z bazy

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -2,32 +2,21 @@
 
 class Main extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('news_model');
+    }
+    
 	public function index()
 	{
             $data['title'] = 'Tytu³';
             $data['description'] = 'To jest przyk³adowy opis.';
             $data['keywords'] = array('klucz1', 'klucz2', 'klucz3');
             $data['lang'] = 'pl';
+            $data['wpisy'] = $this->news_model->pobierz_newsy('strona');
             $this->load->view('szablony/default/header', $data);
-            $this->load->view('szablony/default/main_view');
+            $this->load->view('szablony/default/main_view', $data);
             $this->load->view('szablony/default/footer');
 	}
 }
 
-/* End of file main.php */
-/* Location: ./application/controllers/main.php */
---
+++ application/models/news_model.php
@@ -0,0 +1,24 @@
+<?php
+
+class News_model extends CI_Model {
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+    
+    public function pobierz_newsy($slug='strona', $liga=NULL) {
+        $this->db->order_by('idWpisu', 'desc');
+        if($slug=='strona') {
+            $this->db->where('idLigi', NULL);
+            $this->db->where('idDruzyny', NULL);
+        } elseif($slug=='liga') {
+            $this->db->where('idLigi', $liga);
+        } elseif($slug=='druzyna') {
+            $this->db->where('idDruzyny', $liga);
+        }
+        $query = $this->db->get('wpisy', 3);
+        return $query->result_array();
+    }
+}
+?>
